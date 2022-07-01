8ddb6b5db5d164a398c37b133be56654730c8f7c
Oğuzhan Sadıkoğlu <58795753+Reena-dll@users.noreply.github.com>
2022-07-01 12:25:09 +0300

    uploaded
    uploaded

---
+++ console-koleksiyonlar-ArrayList/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace console_koleksiyonlar_ArrayList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList list = new ArrayList();
+
+            // list.Add("Ayşe");
+            // list.Add(2);
+            // list.Add(true);
+            // list.Add('A');
+
+            // İçerisinde ki verilere erişim
+            //Console.WriteLine(list[1]);
+            // foreach(var item in list)
+            //     Console.WriteLine(item);
+
+            // AddRange
+            Console.WriteLine("****** Add Range ******");
+           // string[] renkler = {"Kırmızı", "Sarı", "Yeşil"};
+            List<int> sayilar = new List<int>(){1,8,3,7,9,92,5};
+           // list.AddRange(renkler);
+            list.AddRange(sayilar);
+
+            foreach(var item in list)
+                Console.WriteLine(item);
+            
+            // Sort
+
+            Console.WriteLine("***** Sort *****");
+            list.Sort();
+            
+            foreach(var item in list)
+                Console.WriteLine(item);
+
+
+            // Binary Search
+            Console.WriteLine("****** Binary Search ******");
+            Console.WriteLine(list.BinarySearch(9));
+
+            // Reverse
+            Console.WriteLine("***** Reverse ******");
+            list.Reverse();
+            foreach(var item in list)
+                Console.WriteLine(item);
+
+            // Clear
+                        Console.WriteLine("***** Clear ******");
+
+            list.Clear();
+            foreach(var item in list)
+                Console.WriteLine(item);
+
+            Console.Read();
+        }
+    }
+}
