df337abd0b7007804634abe4423b898aa2dc9d01
Oğuzhan Sadıkoğlu <58795753+Reena-dll@users.noreply.github.com>
2022-07-02 16:07:30 +0300

    uploaded
    uploaded

---
+++ console-class-staticSinif/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace console_class_staticSinif
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Çalışan Sayısı: {0}", Calisan.CalisanSayisi);
+            Calisan calisan1 = new Calisan("Ayşe", "Yılmaz", "IK");
+            Console.WriteLine("Çalışan Sayısı: {0}", Calisan.CalisanSayisi);
+            Calisan calisan2 = new Calisan("Deniz", "Arda", "IK");
+            Calisan calisan3 = new Calisan("Zikriye", "Ürkmez", "IK");
+            Console.WriteLine("Çalışan Sayısı: {0}", Calisan.CalisanSayisi);
+
+            Console.WriteLine("Toplama İşlemi Sonucu: {0} ",Islemler.Topla(100,200));
+            Console.WriteLine("Çıkarma İşlemi Sonucu: {0} ",Islemler.Cikar(100,200));
+
+            Console.Read();
+        }
+    }
+
+    class Calisan
+    {
+        private static int calisanSayisi;
+
+        public static int CalisanSayisi { get => calisanSayisi; }
+
+        private string Isim;
+        private string Soyisim;
+        private string Departman;
+
+        static Calisan()
+        {
+            calisanSayisi = 0;
+        }
+
+        public Calisan(string isim, string soyisim, string departman)
+        {
+            this.Isim = isim;
+            this.Soyisim = soyisim;
+            this.Departman = departman;
+            calisanSayisi++;
+        }
+    }
+
+    static class Islemler
+    {
+        public static long Topla(int sayi1, int sayi2)
+        {
+            return sayi1 + sayi2;
+        }
+
+        public static long Cikar(int sayi1, int sayi2)
+        {
+            return sayi1 - sayi2;
+        }
+    }
+}
